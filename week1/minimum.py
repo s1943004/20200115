2bc15f6a0d7da62415156683afb9831f6cf606fc
sthancock <steven.hancock@ed.ac.uk>
2020-01-15 12:00:54 +0000

    More comments added.

--- week1/minimum.py
+++ week1/minimum.py
@@ -1,23 +1,26 @@
 
 
 '''
-FUnction to find a minimum
+Function to find a minimum
 '''
 
 import numpy as np
 from datetime import datetime
 
-
+# Make array of data
 arr=np.random.random((100))
 
-t1=datetime.now()
-minN=10000  # a big number
 
+# method 1
+t1=datetime.now()
+
+minN=10000  # a big number
 for i in arr:
   if(i<minN):
     minN=i
 t2=datetime.now()
 
+# method 2
 tt1=datetime.now()
 fastMin=np.min(arr)
 tt2=datetime.now()
